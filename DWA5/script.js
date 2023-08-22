c1ebda89d3540d0df55b6f1f4dfae501e56ce480
dylangangat <dylan.gangat@gmail.com>
2023-08-22 20:54:09 +0200

    feat: Add try...catch logic logic

--- DWA5/script.js
+++ DWA5/script.js
@@ -1,9 +1,41 @@
 const form = document.querySelector('[data-form]');
 const result = document.querySelector('[data-result]');
 
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const entries = new FormData(event.target);
+
+//   try {
+//     const { dividend, divider } = Object.fromEntries(entries);
+
+//     if (!dividend || !divider) throw new Error('Division not performed. Both values are required in inputs. Try again');
+//     if (dividend === 20 && divider < 1) throw new Error('Division not performed. Invalid number provided. Try again');
+//     result.innerText = Math.floor(dividend / divider);
+//   } catch (e) {
+//     result.innerText = e.message;
+//   }
+// });
+
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  try {
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
+    if (dividend === '' || divider === '') {
+      result.innerText = 'Division not performed. Both values are required in inputs. Try again';
+    } else if (!isNaN(dividend) && divider <= 0) {
+      result.innerText = 'Division not performed. Invalid number provided. Try again';
+      console.error(dividend, divider);
+    } else if (isNaN(dividend) || isNaN(divider)) {
+      document.body.innerHTML = '<p style="color: red">Something critical went wrong. Please reload the page</p>';
+
+      throw new Error({ dividend, divider });
+    } else {
+      result.innerText = Math.floor(dividend / divider);
+    }
+  } catch (e) {
+    console.error(e.message);
+  }
 });
