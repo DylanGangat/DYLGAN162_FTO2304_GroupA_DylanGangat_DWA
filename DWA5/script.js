5b819ec500c050493ef788765eeac11c3b4a8cb9
dylangangat <dylan.gangat@gmail.com>
2023-08-23 14:06:27 +0200

    chore: Remove unecessary comments

--- DWA5/script.js
+++ DWA5/script.js
@@ -1,21 +1,6 @@
 const form = document.querySelector('[data-form]');
 const result = document.querySelector('[data-result]');
 
-// form.addEventListener('submit', event => {
-//   event.preventDefault();
-//   const entries = new FormData(event.target);
-
-//   try {
-//     const { dividend, divider } = Object.fromEntries(entries);
-
-//     if (!dividend || !divider) throw new Error('Division not performed. Both values are required in inputs. Try again');
-//     if (dividend === 20 && divider < 1) throw new Error('Division not performed. Invalid number provided. Try again');
-//     result.innerText = Math.floor(dividend / divider);
-//   } catch (e) {
-//     result.innerText = e.message;
-//   }
-// });
-
 form.addEventListener('submit', event => {
   event.preventDefault();
 
@@ -30,7 +15,7 @@ form.addEventListener('submit', event => {
       console.error(dividend, divider);
     } else if (isNaN(dividend) || isNaN(divider)) {
       document.body.innerHTML = '<p style="color: red">Something critical went wrong. Please reload the page</p>';
-
+      
       throw new Error({ dividend, divider });
     } else {
       result.innerText = Math.floor(dividend / divider);
